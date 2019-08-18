ebf5ade4ab80332722cee0fa8553c8a7951d386f
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2019-08-19 01:03:37 +0200

    Allow ff7music and vgmstream plugins to be build
    And place resulting DLLs inside the bin output directory

---
+++ cmake/FindVGMSTREAM.cmake
@@ -0,0 +1,36 @@
+include(FindPackageHandleStandardArgs)
+
+if (NOT VGMSTREAM_FOUND)
+	find_package(FFMPEG REQUIRED)
+
+	find_library(
+		VGMSTREAM_LIBRARY
+		libvgmstream
+		PATH_SUFFIXES
+		lib
+	)
+
+	find_path(
+		VGMSTREAM_INCLUDE_DIR
+		vgmstream.h
+		PATH_SUFFIXES
+		include
+	)
+
+	add_library(VGMSTREAM::VGMSTREAM STATIC IMPORTED)
+
+	set_target_properties(
+		VGMSTREAM::VGMSTREAM
+		PROPERTIES
+		IMPORTED_LOCATION
+		"${VGMSTREAM_LIBRARY}"
+		INTERFACE_INCLUDE_DIRECTORIES
+		"${VGMSTREAM_INCLUDE_DIR}"
+		INTERFACE_LINK_LIBRARIES
+        FFMPEG::SWResample
+	    FFMPEG::AVCodec
+	    FFMPEG::AVFormat
+	)
+
+	find_package_handle_standard_args(VGMSTREAM DEFAULT_MSG VGMSTREAM_LIBRARY VGMSTREAM_INCLUDE_DIR)
+endif()
