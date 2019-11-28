ea5d2dc4c238f12358e9144f61759329dc6e4330
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2019-11-28 17:55:58 +0100

    Fix ffmpeg callback logging
    By printing correctly ffmpeg logs, we avoid also a potential crash happening before because of malformed parameters for trace, debug, info functions

--- src/ffmpeg_movies/ffmpeg_movies.c
+++ src/ffmpeg_movies/ffmpeg_movies.c
@@ -189,16 +189,21 @@ void printStack(CONTEXT* ctx)
 	}
 }
 
-void ffmpeg_log_callback(void* ptr, int level, const char* fmt, va_list vargs)
+void ffmpeg_log_callback(void* ptr, int level, const char* fmt, va_list vl)
 {
+	char msg[4 * 1024]; // 4K
+	static int print_prefix = 1;
+
+	av_log_format_line(ptr, level, fmt, vl, msg, sizeof(msg), &print_prefix);
+
 	switch (level) {
 		case AV_LOG_VERBOSE:
-		case AV_LOG_DEBUG: trace(fmt, vargs); break;
+		case AV_LOG_DEBUG: trace(msg); break;
 		case AV_LOG_INFO:
-		case AV_LOG_WARNING: info(fmt, vargs); break;
+		case AV_LOG_WARNING: info(msg); break;
 		case AV_LOG_ERROR:
 		case AV_LOG_FATAL:
-		case AV_LOG_PANIC: error(fmt, vargs); break;
+		case AV_LOG_PANIC: error(msg); break;
 	}
 
 	if (level <= AV_LOG_WARNING) {
